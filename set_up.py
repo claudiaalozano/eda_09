e97b34957aa3c237a8b309ebef8ecaad6a2f498d
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-12-09 15:36:00 +0000

    muchos cambios

---
+++ set_up.py
@@ -0,0 +1,13 @@
+from itertools import setup
+
+setup(
+    name = 'paquete',
+    version = '1.0',
+    description = 'paquete de prueba',
+    author = 'Claudia'
+    author_email = 'claudia.claudia@gmail.com',
+    url = 'https://github.com/claudia/paquete',
+    package = 'paquete "
+    packages = ['paquete','paquete.hola', "paquete.adios"]
+    
+)
