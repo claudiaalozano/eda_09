5ced3a04006b08acea38b12ca239edb550a6c43c
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-12-09 16:00:04 +0000

    nuevos cambios

--- contador.py
+++ contador.py
@@ -1,1 +1,14 @@
 from collections import Counter
+
+l = [1, 1, 1, ,1 , 2, ,2 , 3]
+c = Counter(l)
+print(l)
+Counter({1:4,2:2, 3:1})
+Counter({"a":3, "r": 2, "s":1, "p": 1, "b": 1})
+c = Counter("palabras")
+print(c)
+animales = "gato perro gato canario perro"
+Counter(animales.split())
+Counter({"gato": 2, "canario": 1, "perro":2})
+
+
