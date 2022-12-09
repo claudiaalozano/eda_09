9702b961a549723957c255b5a172ab36e1599692
claudiaalozano <91722847+claudiaalozano@users.noreply.github.com>
2022-12-09 16:05:50 +0000

    c

--- contador.py
+++ contador.py
@@ -1,6 +1,6 @@
 from collections import Counter
 
-l = [1, 1, 1, ,1 , 2, ,2 , 3]
+l = [1, 1, 1, 1 , 2, 2 , 3]
 c = Counter(l)
 print(l)
 Counter({1:4,2:2, 3:1})
@@ -11,4 +11,24 @@ animales = "gato perro gato canario perro"
 Counter(animales.split())
 Counter({"gato": 2, "canario": 1, "perro":2})
 
+
+animales = "gato perro gato canario perro"
+c = Counter(animales.split())
+
+print(c.most_common(1))
+print(c.most_common(2))
+print(c.most_common(3))
+
+[("perro", 3)]
+[("perro", 3), ("gato", 2)]
+[("perro", 3), ("gato", 2), ("canario", 1)]
+
+l = [10, 20, 30, 40, 10, 20, 30, 10, 20, 10]
+c = Counter(l)
+print(c.items())
+print(c.keys())
+print(c.values())
+print(sum(c.values()))
+
+print(dict(c))
 
